8a0848c488a91b04efa869a0a904b608dec79fd4
binuri <binurim95@gmail.com>
2022-06-10 08:58:01 +0530

    Tested mysql connection with Node

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 
+const mysql = require('mysql');
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'cruddatabase'
+})
+
+
 app.get("/", (req, res)=>{
-    res.send("Hello World");
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('harr12y', 'good movie');"
+    const sqlInsert2 = "SELECT * from movie_reviews;"
+
+    db.query(sqlInsert, (err, result)=>{
+        res.send('Hello World');
+        console.log(error);
+    });
 });
 
 app.listen(3001, () => {
