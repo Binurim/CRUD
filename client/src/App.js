79306cd2b78aa7b7226744e1979848ad9115814b
binuri <binurim95@gmail.com>
2022-06-13 12:12:26 +0530

    Create seperate moviewReview component

--- client/src/App.js
+++ client/src/App.js
@@ -1,102 +1,17 @@
 import './App.css';
-import React, {useState, useEffect} from "react";
-import Axios from "axios";
-
+import React from "react";
+import {Route, Routes} from 'react-router-dom';
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import MovieReview from './components/MovieReview';
 function App() {
-
-  const [movieName, setMovieName] = useState('');
-  const [review, setReview] = useState('');
-  const [movieReviewList, setMovieReviewList] = useState([]);
-  const [newReview, setNewreview] = useState('');
-
-  useEffect(() => {
-    getMovieReviews();
-  }, []);
-
-  const getMovieReviews = () => {
-    Axios.get("http://localhost:3001/api/get").then((response) => {
-      setMovieReviewList(response.data);
-    });
-  }
-
-  const submitReview = () => {
-    Axios.post("http://localhost:3001/api/insert", {
-      movieName: movieName,
-      movieReview: review,
-    });
-    setMovieReviewList([...movieReviewList, {
-      movieName: movieName,
-      movieReview: review,
-    }]);
-    document.getElementById('inputMovie').value = '';
-    document.getElementById('inputReview').value = '';
-  };
-
-  const deleteMovieReview = (movie) => {
-    Axios.delete(`http://localhost:3001/api/delete/${movie}`).then((res) => {
-      getMovieReviews();
-    });
-  };
-
-  const updateMovieReview = (movieName) => {
-    Axios.put("http://localhost:3001/api/update", {
-      movieName: movieName,
-      movieReview: newReview,
-    }).then((res) => {
-      getMovieReviews();
-    });
-    setNewreview('');
-  };
-
   return (
     <div className="App">
-      <h1>CRUD Application</h1>
-      <div className="form">
-        <label>Movie Name:</label>
-        <input
-          type="text"
-          name="movieName"
-          id="inputMovie"
-          onChange={(e) => setMovieName(e.target.value)}
-        />
-        <label>Review:</label>
-        <input
-          type="text"
-          name="review"
-          id="inputReview"
-          onChange={(e) => setReview(e.target.value)}
-        />
-        <button onClick={submitReview}>Submit</button>
-        {movieReviewList.map((value) => {
-          return (
-            <div className="card">
-              <form>
-                <h1> MovieName: {value.movieName}</h1>
-                <p>{value.movieReview}</p>
-                <button
-                  onClick={() => {
-                    deleteMovieReview(value.movieName);
-                  }}
-                >
-                  Delete
-                </button>
-                <input
-                  type="text"
-                  id="updateInput"
-                  onChange={(e) => setNewreview(e.target.value)}
-                />
-                <button
-                  onClick={() => {
-                    updateMovieReview(value.movieName);
-                  }}
-                >
-                  Update
-                </button>
-              </form>
-            </div>
-          );
-        })}
-      </div>
+        <NavBar/>
+            <Routes>
+                <Route path="/" element={<MovieReview/>}></Route>
+            </Routes>
+        <Footer/>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <BrowserRouter>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </BrowserRouter>,
 );
